92ea33a6a1501752bd0c8cda5547180b424615b1
Benjy Hu <491604738@qq.com>
2021-08-10 10:54:01 +0800

    update url

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     description="A small smart package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/BenjyHu/fairy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
